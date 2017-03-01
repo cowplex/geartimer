1c98247a6fc55fd38e880c4ec023c0c2a973debc
Mike Lownds <cowplex@ananke>
2017-03-01 14:20:15 -0500

    Gear app usability improvements
    Added app icons for web app install.
    Added apache file for offline use.
    Changed many things in the app. In no particular order:
    Estimates number of gears deliverable in a match.
    Configurable match timing.
    Monospace timer font, fixed point decimals.
    Added reset button, so the app does not have to be restarted.
    Added ability to save data locally.
    Added driver and notes section to data saved.
    Added ability to save data to an external server.
    Added server passcode to prevent griefing.

--- main.js
+++ main.js
@@ -4,6 +4,13 @@ var app = new Vue({
         gears: [],
         time: 0,
         timer: null,
+		match_time: 135,
+		endgame: 15,
+		driver: null,
+		saved: false,
+		passcode: null,
+		passcode_box: null,
+		notes: null
     },
     methods: {
         gear: function() {
@@ -23,16 +30,82 @@ var app = new Vue({
         },
         stopTimer: function() {
             clearInterval(this.timer)
-        }
-    },
+			this.timer = null;
+        },
+		reset: function() {
+			this.gears = [];
+			this.time = 0;
+			this.timer = null;
+			this.saved = false;
+		},
+		save_passcode: function() {
+			this.passcode = this.passcode_box;
+			localStorage.gear_timer_passcode = this.passcode;
+			this.passcode_box = null;
+		},
+		save_data: function() {
+			this.save_passcode();
+			
+			var current_data = {time:Date.now(), driver:this.driver, notes:this.notes, gears:this.gears};
+			//current_data = JSON.stringify(current_data);
+			//console.log(current_data);
+			
+			var gear_data = [];
+			if(typeof(localStorage.gear_data) != "undefined")
+				gear_data = JSON.parse(localStorage.gear_data);
+			
+			gear_data.push(current_data);
+			localStorage.setItem('gear_data', JSON.stringify(gear_data));
+			
+			this.saved = true;
+		},
+		post_data: function() {
+			if(typeof(localStorage.gear_data) != "undefined")
+				gear_data = JSON.parse(localStorage.gear_data);
+			else
+				return;
+			
+			var xhr = new XMLHttpRequest();   // new HttpRequest instance 
+			xhr.open("POST", "http://team1504.cowplex.com/2017/gear_timer.php");
+			xhr.setRequestHeader("Content-Type", "application/json");
+			//xhr.onreadystatechange = function() {//Call a function when the state changes.
+			//	if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+			//		// Request finished. Do processing here.
+			//	}
+			//}
+			xhr.send(JSON.stringify({passcode:this.passcode, data:gear_data}));
+			
+			/*$.ajax({
+				url: 'http://team1504.cowplex.com/2017/gear_timer.php',
+				data: gear_data,
+				dataType: 'json',
+				success: function (data) {
+					console.log(data);
+				},
+				error: function () {
+					console.log("Error occurred");
+				}
+			});*/
+		},
+		reset_data: function() {
+			//localStorage.gear_timer_passcode = 
+			this.passcode = null;
+			//localStorage.setItem('gear_data', null);
+			
+			localStorage.removeItem('gear_data');
+			localStorage.removeItem('gear_timer_passcode');
+			
+			this.reset();
+		}
+	},
     computed: {
         // a computed getter
-        gpersec: function() {
+        delivery_estimate: function() {
             // `this` points to the vm instance
             if (this.gears.length == 0) {
-                return 0
+                return "?";
             } else {
-                return Math.round((this.gears.length / this.gears[this.gears.length - 1].when) * 100) / 100
+                return (Math.round((this.match_time - this.endgame) * (this.gears.length / this.gears[this.gears.length - 1].when) * 100) / 100).toFixed(1);
             }
         }
     }
